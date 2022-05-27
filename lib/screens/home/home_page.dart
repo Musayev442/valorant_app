12adecea16da71156d46b4c7818b90d8e23a75a9
Musayev442 <musayev442@gmail.com>
2022-05-27 16:36:21 +0400

    video

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 import '../../models/agent.dart';
 import '../../models/services.dart';
+import 'widgets/home_video.dart';
 
+//https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt9037484fa766c7b8/61d90887dea73a236fc56d82/Disruption_VAL_MOBILE_768x490.mp4
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
@@ -20,25 +22,30 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FutureBuilder(
-        future: futureAgent,
-        builder: (BuildContext context, AsyncSnapshot<Agent> snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: snapshot.data?.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    child: Text(
-                        (snapshot.data?.data[index].voiceLine.minDuration)
-                            .toString()),
-                  );
-                });
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
-      ),
+    return Column(
+      children: [
+        HomeVideo(),
+        Center(
+          child: FutureBuilder(
+            future: futureAgent,
+            builder: (BuildContext context, AsyncSnapshot<Agent> snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data?.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        child: Text(
+                            (snapshot.data?.data[index].voiceLine.minDuration)
+                                .toString()),
+                      );
+                    });
+              }
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
+        ),
+      ],
     );
   }
 }
