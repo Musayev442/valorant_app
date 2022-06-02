e03861a685f29645829846ae862b756b88418e6c
Musayev442 <musayev442@gmail.com>
2022-06-02 15:41:11 +0400

    category

---
+++ lib/screens/category/widgets/weapon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Weapon extends StatelessWidget {
+  const Weapon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
