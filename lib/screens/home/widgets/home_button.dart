835b27d119404cb781e52deca4ab8c13cbfca33e
Murad <musayev442@gmail.com>
2022-06-03 22:07:21 +0400

    Fresh update

--- lib/screens/home/widgets/home_button.dart
+++ lib/screens/home/widgets/home_button.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _url = Uri.parse('https://flutter.dev');
 
 class HomeButton extends StatelessWidget {
   const HomeButton(
@@ -31,7 +34,7 @@ class HomeButton extends StatelessWidget {
             ),
             minimumSize: const Size(150, 45), //////// HERE
           ),
-          onPressed: () {},
+          onPressed: _launchUrl,
           child: Text(
             buttonText,
             style: TextStyle(
@@ -43,4 +46,8 @@ class HomeButton extends StatelessWidget {
       ),
     );
   }
+}
+
+void _launchUrl() async {
+  if (!await launchUrl(_url)) throw 'Could not launch $_url';
 }
